693cb4710fe5fc36411271364859b9dc9700a4f5
franklinBen <murod194532@gmail.com>
2022-05-14 19:36:53 +0500

    solution 2

--- reverse_string.go
+++ reverse_string.go
@@ -1,14 +1,10 @@
 package reverse_string
 
-import "fmt"
-
 func ReverseString(input string) (output string) {
 	runes := []rune(input)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
-	for i := 0; i < len(runes); i++ {
-		output = fmt.Sprint(runes[i])
-	}
+	output = string(runes)
 	return output
 }
